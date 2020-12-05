fe9a08ab2641b305c221be7e54c1b7821a7ed008
atacankullabci <atacankullabi@gmail.com>
2020-12-06 00:24:03 +0300

    Track to genre playlist continued

--- src/app/tracks/tracks.component.ts
+++ src/app/tracks/tracks.component.ts
@@ -6,6 +6,7 @@ import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
 import {DialogComponent} from "../shared/dialog/dialog.component";
 import {MatDialog} from "@angular/material/dialog";
+import {IPlaylist} from "../shared/playlist.model";
 
 @Component({
   selector: 'app-tracks',
@@ -17,8 +18,12 @@ export class TracksComponent implements OnInit {
   @Input()
   userId: string;
   mediaContents: IMediaContent[] = [];
+  selectedPlaylists: IPlaylist[] = [];
   selectedGenreList: string[] = [];
   chips = [];
+
+  transferSelected: boolean = false;
+  transferType: string;
 
   dataSource = new MatTableDataSource<MediaContent>();
   displayedColumns: string[] = ['trackName', 'artistName', 'albumName', 'genre'];
@@ -60,14 +65,24 @@ export class TracksComponent implements OnInit {
   }
 
   chipSelected(event) {
+    this.dataSource.filter = '';
+
     const chipName = event.source.value;
     if (event.selected) {
       this.selectedGenreList.push(chipName);
+      this.populatePlaylists(chipName);
     } else {
       const index = this.selectedGenreList.indexOf(chipName, 0);
       this.selectedGenreList.splice(index, 1);
+      this.selectedPlaylists.splice(index, 1);
     }
-    console.log(this.selectedGenreList);
+    console.log(this.selectedPlaylists);
+  }
+
+  populatePlaylists(genre: string) {
+    this.dataSource.filter = genre;
+    this.playlistSelected();
+    this.selectedPlaylists.push({name: genre, mediaContents: this.dataSource.filteredData});
   }
 
   genreChips(mediaContents: IMediaContent[]) {
@@ -82,6 +97,22 @@ export class TracksComponent implements OnInit {
     genreList.forEach((genre) => {
       this.chips.push({state: false, name: genre})
     })
+  }
+
+  selectTransferType(event) {
+    this.transferType = event.value;
+    this.transferSelected = true;
+  }
+
+  playlistSelected() {
+    this.dataSource.filterPredicate = (data: IMediaContent, filter: string): boolean => {
+      const textToSearch = data["genre"] && data["genre"].toLowerCase() || '';
+      return textToSearch.trim().indexOf(filter.trim().toLowerCase()) !== -1;
+    };
+  }
+
+  trackSelected() {
+    this.prepareTable();
   }
 
   applyFilter(event: Event) {
